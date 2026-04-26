d9e4e95dc1b29c73a4268b4a7009450ed571c410
satyam tigunayak <satyamtigunayak10@gmail.com>
2026-04-26 17:11:37 +0530

    Rest api with mongoDB

--- app.js
+++ app.js
@@ -7,33 +7,55 @@ const port=3000;
 const url="mongodb://localhost:27017"
 
 const dbName="studb";
-
-
 const client= new MongoClient(url);
 
 app.use(express.json());
 app.set('view engine','ejs');
 
 
-app.get('/data',async(req,res)=>{
+// app.get('/data',async(req,res)=>{
+//     try{
+// await client.connect()
 
-    try{
-await client.connect()
+// const db=client.db(dbName)
+// const collection= db.collection('students')
+// const data= await collection.find({}).toArray();
 
-const db=client.db(dbName)
-const collection= db.collection('students')
-const data= await collection.find({age:{$gt:22}}).toArray();
+// // res.json(data)
 
-// res.json(data)
+// // send data to ejs template
+// res.render('index',{students:data});
 
-// send data to ejs template
-res.render('index',{students:data});
+//     } catch(err){
+// console.log(err);
+// res.status(500).send({error:"Error occured"})
+//     }
+// })
 
-    } catch(err){
-console.log(err);
-res.status(500).send({error:"Error occured"})
-    }
+// rest api mongodb 🙌❤
+
+let db;
+
+// data base connection function 
+
+async function connectToDB() {
+    await client.connect();
+console.log('connected to server correctly');
+db=client.db(dbName)
+}
+
+app.get('/students',async(req,res)=>{
+    await connectToDB();// ensure db connection
+    const data= await db.collection('students').find({}).toArray();
+    res.json(data)
 })
+
+app.get('/ui',async(req,res)=>{
+    await connectToDB();// ensure db connection
+    const data= await db.collection('students').find({}).toArray();
+    res.render('index',{students:data})
+})
+
 
 app.listen(port,()=>{
     console.log(`server running on ${port}`);
